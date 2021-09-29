a9f25671ed8765e950976258fc7eea531cc81dd9
FaridTavakol <ftavakkolmoghadd@wpi.edu>
2021-09-29 17:31:29 -0400

    Finished and tested the implementation. Everything works as it should.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
 import boundary.TrianglePuzzleApp;
+import controllers.ExitApplicationController;
 import model.Model;
 import model.TrianglePuzzle;
 
@@ -11,9 +17,19 @@ public class Main {
 
 		Model m = new Model();
 		TrianglePuzzle tp = new TrianglePuzzle();
-
 		TrianglePuzzleApp tpa = new TrianglePuzzleApp(m, tp);
+		tpa.addWindowListener(new WindowAdapter()
+		{
+
+			public void windowClosing(WindowEvent e)
+			{
+				new ExitApplicationController(tpa).process();
+			}
+
+		});
+		tpa.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		tpa.setVisible(true);
+
 //	
 	}
 }
